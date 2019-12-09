1b3c9914bca087e4d01db7e5656e87869a9de260
jakerjr <57075422+jakerjr@users.noreply.github.com>
2019-12-09 14:34:12 +0000

    Test

--- src/Book.java
+++ src/Book.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Book {
     private static String title;
@@ -9,88 +10,120 @@ public class Book {
     private static String price;
     private static String pages;
     private static String isbn;
+    private static String info;
 
     public static void run(String filename) {
+        Scanner input = new Scanner(System.in);
+
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-
-            String info;
-
-            while ((info = reader.readLine()) != null) {
-                String[] data = info.split("-");
-
-                if (data.length != 6) {
-                    if (data.length > 6) {
-                        int missing = data.length - 6;
-                        System.out.println("The field is missing " + missing + " piece/s");
-                    } else {
-                        int extra = 6 - data.length;
-                        System.out.println("The field has an extra " + extra + " piece/s. ");
-                    }
-                    System.out.println("-----------------------------");
+            boolean query = false;
+            System.out.println("Do you want to search for a specific author or see all books? \n<Enter the word \"author\" or \"all\">");
+            while (!query) {
+                String answer = input.next().toLowerCase();
+                if (answer.equals("author")){
+                    System.out.println("Enter the name of the author you want to find");
+                    String authorName = input.next();
+                    requirement4(reader, authorName);
+                    query = true;
+                } else if (answer.equals("all")){
+                    requirement(reader);
+                    query = true;
                 } else {
-                    if (!data[0].trim().isEmpty()) {
-                        title = data[0];
-                    } else {
-                        title = "Book title is missing.";
-                    }
-
-                    if (!data[1].trim().isEmpty()) {
-                        author = data[1].trim();
-                    } else {
-                        author = "Book author is missing.";
-                    }
-
-                    if (!data[2].trim().isEmpty()) {
-                        publisher = data[2].trim();
-                    } else {
-                        publisher = "Book publisher is missing.";
-                    }
-
-                    if (isDouble(data[3])) {
-                        price = data[3].trim();
-                    } else {
-                        price = "Book price is not a numeric value.";
-                    }
-
-                    if (isInteger(data[4])) {
-                        pages = data[4].trim();
-                    } else {
-                        pages = "Book price is not a numeric value.";
-                    }
-
-                    if (!data[5].trim().isEmpty()) {
-                        isbn = data[5].trim();
-                    } else {
-                        isbn = "Book ISBN is missing.";
-                    }
-
-//                    System.out.println(title + "\n" + author + "\n" + publisher + "\n" + price + "\n" + pages + "\n" + isbn);
-                    System.out.format("%-15s %s", "Title: ", title);
-                    System.out.format("%-16s %s", "\nAuthor: ", author);
-                    System.out.format("%-16s %s", "\nPublisher: ", publisher);
-                    System.out.format("%-16s %s", "\nPrice: ", price);
-                    System.out.format("%-16s %s", "\nPages: ", pages);
-                    System.out.format("%-16s %s", "\nISBN: ", isbn);
-                    System.out.println("\n-----------------------------");
+                    System.out.println("That may not be a valid option. Please check your spelling");
                 }
             }
+            requirement(reader);
+
         } catch (IOException e) {
             System.out.println("File not found.");
         }
     }
 
-    private static boolean isInteger(String number) {
+    public static void requirement(BufferedReader reader) throws IOException{
+        while ((info = reader.readLine()) != null) {
+            String[] data = info.split("-");
+            details(data);
+        }
+    }
+
+    public static void requirement4(BufferedReader reader, String authorNameInput) throws IOException {
+        while ((info = reader.readLine()) != null){
+            String[] data = info.split("-");
+            if (data[1].contains(authorNameInput)){
+                details(data);
+            }
+        }
+    }
+
+    public static void details(String[] data) {
+        if (data.length != 6) {
+            if (data.length > 6) {
+                int missing = data.length - 6;
+                System.out.println("The field is missing " + missing + " piece/s");
+            } else {
+                int extra = 6 - data.length;
+                System.out.println("The field has an extra " + extra + " piece/s. ");
+            }
+            System.out.println("-----------------------------");
+        } else {
+            if (!data[0].trim().isEmpty()) {
+                title = data[0];
+            } else {
+                title = "Book title is missing.";
+            }
+
+            if (!data[1].trim().isEmpty()) {
+                author = data[1].trim();
+            } else {
+                author = "Book author is missing.";
+            }
+
+            if (!data[2].trim().isEmpty()) {
+                publisher = data[2].trim();
+            } else {
+                publisher = "Book publisher is missing.";
+            }
+
+            if (isDouble(data[3])) {
+                price = data[3].trim();
+            } else {
+                price = "Book price is not a numeric value.";
+            }
+
+            if (isInteger(data[4])) {
+                pages = data[4].trim();
+            } else {
+                pages = "Book price is not a numeric value.";
+            }
+
+            if (!data[5].trim().isEmpty()) {
+                isbn = data[5].trim();
+            } else {
+                isbn = "Book ISBN is missing.";
+            }
+
+            System.out.format("%-15s %s", "Title: ", title);
+            System.out.format("%-16s %s", "\nAuthor: ", author);
+            System.out.format("%-16s %s", "\nPublisher: ", publisher);
+            System.out.format("%-16s %s", "\nPrice: ", price);
+            System.out.format("%-16s %s", "\nPages: ", pages);
+            System.out.format("%-16s %s", "\nISBN: ", isbn);
+            System.out.println("\n-----------------------------");
+        }
+    }
+
+    public static boolean isInteger(String number) {
         try {
-            int test = Integer.parseInt(number.trim());
+            int checkInteger = Integer.parseInt(number.trim());
             return true;
         } catch (NumberFormatException e) {
             return false;
         }
     }
 
-    private static boolean isDouble(String number) {
+    public static boolean isDouble(String number) {
         try {
-            double test = Double.parseDouble(number);
+            double checkDouble = Double.parseDouble(number);
             return true;
         } catch (NumberFormatException e) {
             return false;
--- src/c3573954.java
+++ src/c3573954.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class c3573954 {
     public static void main(String[] args) {
         Book book = new Book();
-        book.run("valid-invalid.txt");
+        Scanner input = new Scanner(System.in);
+        String fileName = input.next();
+        book.run(fileName);
     }
 }
